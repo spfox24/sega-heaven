211649e47755f388ad02685fbaa9d02696a23d58
spfox24 <68411909+spfox24@users.noreply.github.com>
2020-10-15 00:43:11 -0500

    adds html, css, js contents

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 // Constants
 
+const PLAT_URL = 'https://api.rawg.io/api/platforms'
+const GAME_URL = 'https://api.rawg.io/api/games?platforms=167,107,119,117,74,106&search='
 
 
 // Variables
+let gameData;
 
 
 
 // Cached Element References
+
 
 
 
@@ -15,3 +19,70 @@
 
 
 // Functions
+
+init();
+
+function init() {
+    getPlatData();
+}
+
+function getData() {
+    $.ajax(GAME_URL + userInput)
+    .then(function(data){
+        console.log('Game Data: ', data);
+    }, function(error) {
+        console.log('Error: ', error);
+    });
+}
+
+function getPlatData() {
+    $.ajax(PLAT_URL)
+    .then(function(data){
+        console.log('Platform Data: ', data);
+    }, function(error) {
+        console.log('Error: ', error);
+    });
+}
+
+
+
+/*
+function generateUI() {
+    return systemData.results.map(function(system){
+        return `<article data-url="${system.url}" class="system flex-ctr">
+                    <h3>${system.name}</h3>
+                </article>`;
+    });
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+init();
+
+function init() {
+    getData();
+}
+
+function getData() {
+
+    const url = BASE_URL
+
+    $.ajax(url)
+    .then(function(data) {
+        
+        artistData = data;
+    console.log(data);
+    });
+}
+*/
