1b314cfe08cd0aaa2a682e0bc3d62db1eba32e31
spfox24 <68411909+spfox24@users.noreply.github.com>
2020-10-15 23:21:56 -0500

    adds game search

--- js/script.js
+++ js/script.js
@@ -1,82 +1,108 @@
 // Constants
 
 const PLAT_URL = 'https://api.rawg.io/api/platforms'
-const GAME_URL = 'https://api.rawg.io/api/games?platforms=167,107,119,117,74,106&search='
+const GAME_URL = 'https://api.rawg.io/api/games'
 
 
 // Variables
-let gameData, systemData;
+let gameData, systemData, userInput, year;
 
 
 
 // Cached Element References
-const $sysEl = $('.system');
+
+// GAME_URL El
+const $gameEl = $('#gameList');
 
 
+// PLAT_URL El
+const $master = $('#master');
+const $genesis = $('#genesis');
+const $thirtytwo = $('#thirtytwo');
+const $cd = $('#cd');
+const $saturn = $('#saturn');
+const $dream = $('#dreamcast');
+
+// Form El
+const $form = $('form')
+const $input = $('input[type="text"]');
+
+const yearEl = document.getElementById('year');
 
 // Event Listeners
-$sysEl.on('click', handleClick);
-
+$form.on('submit', handleGetData);
 
 // Functions
 
-function handleClick() {
-    $.ajax(PLAT_URL)
-    .then(function(data){
-        console.log('Platform Data: ', data);
-    }, function(error) {
-        console.log('Error: ', error);
-    });
-}
-
-
-function getData() {
-    $.ajax(GAME_URL + userInput)
-    .then(function(data){
-        console.log('Game Data: ', data);
-    }, function(error) {
-        console.log('Error: ', error);
-    });
-}
-
-
-/*
-function generateUI() {
-    return systemData.results.map(function(system){
-        return `<article data-url="${system.url}" class="system flex-ctr">
-                    <h3>${system.name}</h3>
-                </article>`;
-    });
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 init();
+getYear();
 
 function init() {
-    getData();
-}
+    getSysData();
+};
 
-function getData() {
+function handleGetData(event) {
+    event.preventDefault();
 
-    const url = BASE_URL
+    userInput = $input.val();
+    
+    if(!userInput) return;
 
-    $.ajax(url)
-    .then(function(data) {
-        
-        artistData = data;
-    console.log(data);
+    $.ajax(GAME_URL + '?platforms=167,107,119,117,74,106&search=' + userInput)
+    .then(function(data){
+
+        gameData = data;
+        // console.log(gameData);
+
+        $input.val('');
+        render();
+
+    }, function(error) {
+        console.log('Error: ', error);
     });
 }
-*/
+
+function getSysData() {
+    $.ajax(PLAT_URL)
+    .then(function(data){
+        
+        systemData = data;
+
+        render();
+
+    }, function(error) {
+        console.log('Error: ', error);
+    });
+}
+
+function generateUI() {
+    return gameData.results.map(function(game) {
+        return `
+        <article id="games" class="flex-ctr">
+            <ul>
+               <li>${game.name}</li>
+            </ul>
+        </article>`;
+    });
+}
+
+function render() {
+
+    
+// Render System Name to Main
+    $master.text(systemData.results[44].name);
+    $genesis.text(systemData.results[40].name);
+    $thirtytwo.text(systemData.results[43].name);
+    $cd.text(systemData.results[42].name);
+    $saturn.text(systemData.results[41].name); 
+    $dream.text(systemData.results[45].name); 
+    
+// Render Games to Main
+    $gameEl.html(generateUI());
+
+};
+
+function getYear() {
+    year = new Date().getFullYear();
+    yearEl.innerText = year;
+}
