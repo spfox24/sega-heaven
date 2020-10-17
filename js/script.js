9e82fe2b1ea53688ca55cdafef0ac4286a835e63
spfox24 <68411909+spfox24@users.noreply.github.com>
2020-10-17 01:44:19 -0500

    adds modal function and css

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const PLAT_URL = 'https://api.rawg.io/api/platforms'
 const GAME_URL = 'https://api.rawg.io/api/games'
 
 
+
 // Variables
 let gameData, systemData, userInput, year;
 
@@ -23,6 +24,26 @@ const $cd = $('#cd');
 const $saturn = $('#saturn');
 const $dream = $('#dreamcast');
 
+// PLAT DESCRIPTION El
+const $modMas = $('#master-mod');
+const $btnMas = $('.btn-mas');
+
+const $modGen = $('#genesis-mod');
+const $btnGen = $('.btn-gen');
+
+const $modSat = $('#saturn-mod');
+const $btnSat = $('.btn-sat');
+
+const $modCD = $('#cd-mod');
+const $btnCD = $('.btn-cd');
+
+const $mod32 = $('#32-mod');
+const $btn32 = $('.btn-32');
+
+const $modDC = $('#dreamcast-mod');
+const $btnDC = $('.btn-dc');
+
+
 // Form El
 const $form = $('form')
 const $input = $('input[type="text"]');
@@ -31,6 +52,13 @@ const yearEl = document.getElementById('year');
 
 // Event Listeners
 $form.on('submit', handleGetData);
+$btnMas.on('click', showMaster);
+$btnGen.on('click', showGenesis);
+$btnSat.on('click', showSaturn);
+$btnCD.on('click', showCD);
+$btn32.on('click', show32);
+$btnDC.on('click', showDC);
+
 
 // Functions
 
@@ -40,6 +68,19 @@ getYear();
 function init() {
     getSysData();
 };
+
+function getSysData() {
+    $.ajax(PLAT_URL)
+    .then(function(data){
+        
+        systemData = data;
+
+        render();
+
+    }, function(error) {
+        console.log('Error: ', error);
+    });
+}
 
 function handleGetData(event) {
     event.preventDefault();
@@ -52,22 +93,8 @@ function handleGetData(event) {
     .then(function(data){
 
         gameData = data;
-        // console.log(gameData);
 
         $input.val('');
-        render();
-
-    }, function(error) {
-        console.log('Error: ', error);
-    });
-}
-
-function getSysData() {
-    $.ajax(PLAT_URL)
-    .then(function(data){
-        
-        systemData = data;
-
         render();
 
     }, function(error) {
@@ -87,20 +114,50 @@ function generateUI() {
 }
 
 function render() {
-
     
-// Render System Name to Main
-    $master.text(systemData.results[44].name);
-    $genesis.text(systemData.results[40].name);
-    $thirtytwo.text(systemData.results[43].name);
-    $cd.text(systemData.results[42].name);
-    $saturn.text(systemData.results[41].name); 
-    $dream.text(systemData.results[45].name); 
-    
-// Render Games to Main
+    // Render System Name to Main
+        $master.text(systemData.results[44].name);
+        $genesis.text(systemData.results[40].name);
+        $thirtytwo.text(systemData.results[43].name);
+        $cd.text(systemData.results[42].name);
+        $saturn.text(systemData.results[41].name); 
+        $dream.text(systemData.results[45].name); 
+        
+    // Render Games to Main
     $gameEl.html(generateUI());
+    };
 
-};
+function showMaster(event) {
+    event.preventDefault();
+    $modMas.modal();
+}
+
+function showGenesis(event) {
+    event.preventDefault();
+    $modGen.modal();
+}
+
+function showSaturn(event) {
+    event.preventDefault();
+    $modSat.modal();
+}
+
+function showCD(event) {
+    event.preventDefault();
+    $modCD.modal();
+}
+
+function show32(event) {
+    event.preventDefault();
+    $mod32.modal();
+}
+
+function showDC(event) {
+    event.preventDefault();
+    $modDC.modal();
+}
+
+
 
 function getYear() {
     year = new Date().getFullYear();
